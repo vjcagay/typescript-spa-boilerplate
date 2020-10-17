65342a151fd5265f48be248c83e46e854b356ac8
Vincent John Cagay <me@vjcagay.com>
2020-10-17 23:30:38 +0900

    :art: Update Welcome screen

--- src/ts/App.tsx
+++ src/ts/App.tsx
@@ -1,33 +1,49 @@
 import React from "react";
-import { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 import { reset } from "styled-reset";
 
 // Import fonts manually since styled-components does not support bundling
-import "../fonts/Roboto-Regular.ttf";
+import "../fonts/Inter-Regular.ttf";
+import "../fonts/Inter-Bold.ttf";
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
 
   @font-face {
-	font-family: "Roboto";
-	src: url("./Roboto-Regular.ttf");
-	font-display: fallback;
+    font-family: "Inter";
+    font-weight: regular;
+    src: url("./Inter-Regular.ttf");
+    font-display: fallback;
+  }
+
+  @font-face {
+    font-family: "Inter";
+    font-weight: bold;
+    src: url("./Inter-Bold.ttf");
+    font-display: fallback;
   }
 
   body {
-	font-family: "Roboto";
-	font-size: 2em;
-	text-align: center;
+    font-family: "Inter";
+    text-align: center;
+    background-color: #00273f;
+    color: #ffffff;
   }
+`;
+
+const RootDiv = styled.div`
+  height: 100vh;
+  height: -webkit-fill-available;
+  display: grid;
 `;
 
 import Message from "./Message";
 
 const App = () => (
-  <>
+  <RootDiv>
     <GlobalStyle />
     <Message />
-  </>
+  </RootDiv>
 );
 
 export default App;
--- src/ts/Message.tsx
+++ src/ts/Message.tsx
@@ -2,9 +2,33 @@ import React from "react";
 import styled from "styled-components";
 
 const MessageStyled = styled.div`
-  text-transform: uppercase;
+  margin: auto;
+  padding: 3em;
+
+  img {
+    width: 75%;
+  }
+
+  h1 {
+    margin: 1em 0;
+    font-size: 2.5em;
+    font-weight: bold;
+  }
+
+  p {
+    line-height: 1.5;
+  }
 `;
 
-const MessageComponent = () => <MessageStyled>Hello World of TypeScript!</MessageStyled>;
+const MessageComponent = () => (
+  <MessageStyled>
+    <img src="https://64.media.tumblr.com/a8d91d9d6f197e126351464305f75dd7/tumblr_msitq5qMwW1sg2d49o1_1280.gif" />
+    <br />
+    <h1>Hello World!</h1>
+    <p>You can now start building your application with TypeScript.</p>
+    <br />
+    <p>Happy coding!</p>
+  </MessageStyled>
+);
 
 export default MessageComponent;
